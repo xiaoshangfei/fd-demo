787a303cfa076950e4f708647682c5ffcd538c69
xiaoshangfei <fei523688393@hotmail.com>
2023-05-11 15:56:19 +0800

    字面量props导致的问题

--- packages/vue2-demo/src/router/index.js
+++ packages/vue2-demo/src/router/index.js
@@ -15,48 +15,6 @@ const routes = [
     name: 'pass-object-literals-as-props',
     meta: { title: 'props传递字面量对象', description: '父组件响应式数据导致子组件重复渲染' },
     component: () => import('@/views/PassObjectLiteralsAsProps')
-  },
-  {
-    path: '/pass-object-literals-as-props1',
-    name: 'pass-object-literals-as-props1',
-    meta: { title: 'props传递字面量对象', description: '父组件响应式数据导致子组件重复渲染' },
-    component: () => import('@/views/PassObjectLiteralsAsProps')
-  },
-  {
-    path: '/pass-object-literals-as-props2',
-    name: 'pass-object-literals-as-props2',
-    meta: { title: 'props传递字面量对象', description: '父组件响应式数据导致子组件重复渲染' },
-    component: () => import('@/views/PassObjectLiteralsAsProps')
-  },
-  {
-    path: '/pass-object-literals-as-props3',
-    name: 'pass-object-literals-as-props3',
-    meta: { title: 'props传递字面量对象', description: '父组件响应式数据导致子组件重复渲染' },
-    component: () => import('@/views/PassObjectLiteralsAsProps')
-  },
-  {
-    path: '/pass-object-literals-as-props4',
-    name: 'pass-object-literals-as-props4',
-    meta: { title: 'props传递字面量对象', description: '父组件响应式数据导致子组件重复渲染' },
-    component: () => import('@/views/PassObjectLiteralsAsProps')
-  },
-  {
-    path: '/pass-object-literals-as-props5',
-    name: 'pass-object-literals-as-props5',
-    meta: { title: 'props传递字面量对象', description: '父组件响应式数据导致子组件重复渲染' },
-    component: () => import('@/views/PassObjectLiteralsAsProps')
-  },
-  {
-    path: '/pass-object-literals-as-props6',
-    name: 'pass-object-literals-as-props6',
-    meta: { title: 'props传递字面量对象', description: '父组件响应式数据导致子组件重复渲染' },
-    component: () => import('@/views/PassObjectLiteralsAsProps')
-  },
-  {
-    path: '/pass-object-literals-as-props7',
-    name: 'pass-object-literals-as-props7',
-    meta: { title: 'props传递字面量对象', description: '父组件响应式数据导致子组件重复渲染' },
-    component: () => import('@/views/PassObjectLiteralsAsProps')
   }
 ];
 
--- packages/vue2-demo/src/views/PassObjectLiteralsAsProps/ChildBlock.vue
+++ packages/vue2-demo/src/views/PassObjectLiteralsAsProps/ChildBlock.vue
@@ -1,7 +1,26 @@
 <template>
-  <div></div>
+  <div class="flex h-16 w-4/5 bg-orange-300 rounded-md shadow-sm items-center justify-center text-lg">子组件渲染次数：{{ renderCount }}</div>
 </template>
 <script>
-export default {};
+export default {
+  props: {
+    obj: {
+      type: Object,
+      default: () => null
+    }
+  },
+  data() {
+    return {
+      renderCount: 1
+    };
+  },
+  watch: {
+    obj: {
+      handler() {
+        this.renderCount++;
+      }
+    }
+  }
+};
 </script>
 <style></style>
